ec28cb1d579c7b6e57f6d2c05048cb7ef8d7bd6e
Andrés Chacón Miranda <28637127+achaconm2001@users.noreply.github.com>
2024-02-28 18:43:10 -0600

    Update VenusDance animation with increased scale and total duration

--- venus_dance.py
+++ venus_dance.py
@@ -1,23 +1,24 @@
 from math import radians
-from manimlib import *
+from manim import *
 
 
-SCALE = 1
+SCALE = 2
+TOTAL_DURATION = 30
 
 
 class VenusDance(Scene):
     def construct(self):
         plane = NumberPlane()
-        self.play(ShowCreation(plane))
+        self.play(Create(plane))
         self.wait()
 
         sun = Dot(color=YELLOW).move_to([0, 0, 0])
         venus = Dot(color=YELLOW_E).move_to([1 * SCALE, 0, 0])
         earth = Dot(color=BLUE).move_to([1.5 * SCALE, 0, 0])
 
-        self.play(ShowCreation(sun))
-        self.play(ShowCreation(venus))
-        self.play(ShowCreation(earth))
+        self.play(Create(sun))
+        self.play(Create(venus))
+        self.play(Create(earth))
         self.wait()
 
         tracker = ValueTracker(0)
@@ -30,13 +31,17 @@ class VenusDance(Scene):
 
             tracker.set_value(0)
             line = Line(start=earth.get_center(),
-                        end=venus.get_center(), stroke_width=0.1)
+                        end=venus.get_center(), stroke_width=0.5)
             self.add(line)
 
         earth.add_updater(create_line)
 
         seconds = 30
+        venus_rotations = 8
+        earth_rotations = 13
 
-        self.play(Rotating(venus, angle=TAU * 13, about_point=[0, 0, 0], run_time=seconds), Rotating(
-            earth, angle=TAU * 8, about_point=[0, 0, 0], run_time=seconds))
+        self.play(
+            Rotating(venus, radians=TAU * venus_rotations, about_point=[0, 0, 0], rate_func=linear, run_time=seconds), 
+            Rotating(earth, radians=TAU * earth_rotations, about_point=[0, 0, 0], rate_func=linear, run_time=seconds)
+        )
         self.wait()
